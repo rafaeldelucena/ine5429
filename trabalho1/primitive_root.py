cb245f98063f8b369f750a6fc48b2e941e34e45a
Rafael de Lucena Valle <rafaeldelucena@gmail.com>
2014-10-07 14:46:09 -0300

    Adding .tex report

--- trabalho1/primitive_root.py
+++ trabalho1/primitive_root.py
@@ -1,12 +1,17 @@
 import math
 import sys
 
+"""
+Funcao necessaria para criacao de limites com numeros grandes
+"""
 def bigRange(start, stop, step):
     i = start
     while i < stop:
         yield i
         i += step
-
+"""
+Dado um valor de entrada N, retorna uma colecao de fatores primos
+"""
 def primeFactors(n):
     f, fs = 3, set()
     while n % 2 == 0:
@@ -20,26 +25,26 @@ def primeFactors(n):
     if n > 1: fs.add(n)
     return fs
 
+"""
+Dao um numero primo P, encontra os fatores primos de P - 1 e calcula a condicao para raiz primitiva modulo P de um numero A entre 2 ate P - 1.
+"""
 def primitiveRoots(number):
     roots = []
     if (number < 2):
-        return roots
+        yield []
     phi = number - 1
-    factors = primeFactors(phi)
     for m in bigRange(2, phi, 1):
         is_root = True
-        for p in factors:
+        for p in primeFactors(phi):
             if pow(m, int(phi / p), number) == 1:
                 is_root = False
         if (is_root):
-            roots.append(m)
-    return roots
-
-def main():
-    number = int(raw_input("Enter a prime number: "))
-    roots = primitiveRoots(number)
-    if (roots):
-        print("Find ", len(roots), " primitive roots module ", number, " = ", roots)
+            yield m
 
 if __name__ == "__main__":
-    sys.exit(main())
+    number = int(raw_input("Enter a prime number: "))
+    for g in primitiveRoots(number):
+        print "Find a primitive root module", number, " = ", g 
+        next = raw_input("Find next? (y/N) ")
+        if next != ("y" or "Y"):
+            sys.exit();
